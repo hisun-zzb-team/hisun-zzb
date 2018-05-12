8ee1cc92c57efa0200b057668e62800f7418398f
ZHT <605144321@qq.com>
2018-05-12 14:53:29 +0800

    提交添加材料代码

---
+++ src/main/java/com/hisun/saas/zzb/dzda/mlcl/controller/E01Z1Controller.java
@@ -0,0 +1,278 @@
+/*
+ * Copyright (c) 2018. Hunan Hisun Union Information Technology Co, Ltd. All rights reserved.
+ * http://www.hn-hisun.com
+ * 注意:本内容知识产权属于湖南海数互联信息技术有限公司所有,除非取得商业授权,否则不得用于商业目的.
+ */
+
+package com.hisun.saas.zzb.dzda.mlcl.controller;
+
+import com.google.common.collect.Maps;
+import com.hisun.base.controller.BaseController;
+import com.hisun.base.dao.util.CommonConditionQuery;
+import com.hisun.base.dao.util.CommonOrder;
+import com.hisun.base.dao.util.CommonOrderBy;
+import com.hisun.base.dao.util.CommonRestrictions;
+import com.hisun.base.exception.GenericException;
+import com.hisun.base.vo.PagerVo;
+import com.hisun.saas.sys.admin.dzda.entity.ECatalogTypeInfo;
+import com.hisun.saas.sys.admin.dzda.service.ECatalogTypeService;
+import com.hisun.saas.sys.auth.UserLoginDetails;
+import com.hisun.saas.sys.auth.UserLoginDetailsUtil;
+import com.hisun.saas.sys.log.LogOperateType;
+import com.hisun.saas.sys.log.RequiresLog;
+import com.hisun.saas.sys.taglib.treeTag.TreeNode;
+import com.hisun.saas.sys.util.EntityWrapper;
+import com.hisun.saas.zzb.dzda.a38.service.A38Service;
+import com.hisun.saas.zzb.dzda.mlcl.service.E01Z1Service;
+import com.hisun.saas.zzb.dzda.mlcl.vo.E01Z1Vo;
+import com.hisun.saas.zzb.dzda.mlcl.entity.E01Z1;
+import com.hisun.util.StringUtils;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+* @author liuzj {279421824@qq.com}
+*/
+@Controller
+@RequestMapping("/zzb/dzda/e01z1")
+public class E01Z1Controller extends BaseController {
+
+    @Resource
+    private ECatalogTypeService eCatalogTypeService;
+    @Resource
+    private A38Service a38Service;
+    @Resource
+    private E01Z1Service e01Z1Service;
+
+    @RequiresLog(operateType = LogOperateType.UPDATE,description = "修改材料目录:${vo.e01Z111}")
+    @RequiresPermissions("catalogType:*")
+    @RequestMapping(value = "/update")
+    public @ResponseBody Map<String, Object> update(E01Z1Vo vo,HttpServletRequest request) throws GenericException {
+        Map<String, Object> map = new HashMap<String, Object>();
+        String currentNodeId = StringUtils.trimNull2Empty(request.getParameter("currentNodeId"));
+        ECatalogTypeInfo eCatalogTypeInfo = new ECatalogTypeInfo();
+        eCatalogTypeInfo=eCatalogTypeService.getByPK(currentNodeId);
+        String currentNodeName = eCatalogTypeInfo.getCatalogValue();
+        String id = StringUtils.trimNull2Empty(request.getParameter("id"));
+        vo.setE01Z101B(currentNodeId);
+        vo.setE01Z101A(currentNodeName);
+        try {
+
+            UserLoginDetails userLoginDetails = UserLoginDetailsUtil.getUserLoginDetails();
+            E01Z1 e01Z1 = this.e01Z1Service.getByPK(id);
+            String oldPid = "";
+            int oldSort = e01Z1.getE01Z104();
+            if(e01Z1.getA38()!=null){
+                oldPid = e01Z1.getA38().getId();
+            }
+
+            BeanUtils.copyProperties(e01Z1, vo);
+            e01Z1.setA38(this.a38Service.getByPK(e01Z1.getA38().getId()));
+            EntityWrapper.wrapperUpdateBaseProperties(e01Z1,userLoginDetails);
+            this.e01Z1Service.updateE01Z1(e01Z1, oldPid, oldSort);
+            map.put("success", true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e);
+            throw new GenericException(e.getMessage());
+        }
+        return map;
+    }
+
+    @RequestMapping(value = "/ajax/editMlcl")
+    public @ResponseBody ModelAndView editMlcl(HttpServletRequest request){
+        Map<String, Object> map = new HashMap<String, Object>();
+        String id = StringUtils.trimNull2Empty(request.getParameter("id"));
+        E01Z1 e01Z1 = e01Z1Service.getByPK(id);
+        String currentNodeId = StringUtils.trimNull2Empty(request.getParameter("currentNodeId"));
+        String currentNodeName = StringUtils.trimNull2Empty(request.getParameter("currentNodeName"));
+        String currentNodeParentId = StringUtils.trimNull2Empty(request.getParameter("currentNodeParentId"));
+        String a38Id = StringUtils.trimNull2Empty(request.getParameter("a38Id"));
+        map.put("currentNodeId",currentNodeId);
+        map.put("currentNodeName",currentNodeName);
+        map.put("catalogTypeName",e01Z1.getE01Z101A());
+        map.put("currentNodeParentId",currentNodeParentId);
+        map.put("a38Id",a38Id);
+        map.put("vo",e01Z1);
+        return new ModelAndView("saas/zzb/daDemo/manage/editMlcl",map);
+    }
+
+    @RequiresLog(operateType = LogOperateType.SAVE,description = "增加材料:${vo.e01Z111}")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public @ResponseBody Map<String, Object> save(E01Z1Vo vo,HttpServletRequest request) throws GenericException {
+        Map<String, Object> map = new HashMap<String, Object>();
+        String currentNodeId = StringUtils.trimNull2Empty(request.getParameter("currentNodeId"));
+        String currentNodeName = StringUtils.trimNull2Empty(request.getParameter("currentNodeName"));
+        String a38Id = StringUtils.trimNull2Empty(request.getParameter("a38Id"));
+        ECatalogTypeInfo eCatalogTypeInfo = new ECatalogTypeInfo();
+        eCatalogTypeInfo=eCatalogTypeService.getByPK(currentNodeId);
+        currentNodeName = eCatalogTypeInfo.getCatalogValue();
+        try {
+            UserLoginDetails userLoginDetails = UserLoginDetailsUtil.getUserLoginDetails();
+            E01Z1 e01Z1 = new E01Z1();
+            BeanUtils.copyProperties(e01Z1, vo);
+            e01Z1.setE01Z101B(currentNodeId);
+            e01Z1.setE01Z101A(currentNodeName);
+            if(StringUtils.isNotBlank(a38Id)){
+                e01Z1.setA38(this.a38Service.getByPK(a38Id));
+            }
+            EntityWrapper.wrapperSaveBaseProperties(e01Z1,userLoginDetails);
+            e01Z1Service.save(e01Z1);
+            map.put("success", true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e);
+            throw new GenericException(e);
+        }
+
+        return map;
+    }
+
+    @RequestMapping(value = "/ajax/addMlcl")
+    public @ResponseBody ModelAndView addMlcl(HttpServletRequest request){
+        Map<String, Object> map = Maps.newHashMap();
+        String currentNodeId = StringUtils.trimNull2Empty(request.getParameter("currentNodeId"));
+        String currentNodeName = StringUtils.trimNull2Empty(request.getParameter("currentNodeName"));
+        String currentNodeParentId = StringUtils.trimNull2Empty(request.getParameter("currentNodeParentId"));
+        String a38Id = StringUtils.trimNull2Empty(request.getParameter("a38Id"));
+
+        E01Z1Vo vo = new E01Z1Vo();
+        vo.setE01Z101B(currentNodeId);
+        vo.setE01Z101A(currentNodeName);
+
+        int sort = this.e01Z1Service.getMaxSort(a38Id,currentNodeId);
+
+        vo.setE01Z114(2);
+        vo.setE01Z124(1);
+        vo.setE01Z107(1);
+        ECatalogTypeInfo eCatalogTypeInfo = new ECatalogTypeInfo();
+        eCatalogTypeInfo=eCatalogTypeService.getByPK(currentNodeId);
+        currentNodeName = eCatalogTypeInfo.getCatalogValue();
+        map.put("currentNodeId",currentNodeId);
+        map.put("currentNodeName",currentNodeName);
+        map.put("currentNodeParentId",currentNodeParentId);
+        map.put("a38Id",a38Id);
+        map.put("vo",vo);
+        return new ModelAndView("saas/zzb/daDemo/manage/addMlcl",map);
+    }
+
+    @RequestMapping(value = "/ajax/mlxxList")
+    public @ResponseBody ModelAndView mlxxList(HttpServletRequest request,
+                                               @RequestParam(value="pageNum",defaultValue="1")int pageNum,
+                                               @RequestParam(value="pageSize",defaultValue="10") int pageSize) throws GenericException {
+        Map<String, Object> map = Maps.newHashMap();
+        String currentNodeId = StringUtils.trimNull2Empty(request.getParameter("currentNodeId"));
+        String currentNodeName = StringUtils.trimNull2Empty(request.getParameter("currentNodeName"));
+        String currentNodeParentId = StringUtils.trimNull2Empty(request.getParameter("currentNodeParentId"));
+        String a38Id = StringUtils.trimNull2Empty(request.getParameter("a38Id"));
+        boolean isParent = false;
+
+        try{
+
+            CommonConditionQuery query = new CommonConditionQuery();
+
+            if(StringUtils.isEmpty(currentNodeParentId)){
+                query.add(CommonRestrictions.and(" a38.id = :id ", "id", a38Id));
+            }else {
+                query.add(CommonRestrictions.and(" a38.id = :id ", "id", a38Id));
+                query.add(CommonRestrictions.and(" e01z101b = :e01z101b ", "e01z101b", currentNodeId));
+            }
+
+            Long total = this.e01Z1Service.count(query);
+            CommonOrderBy orderBy = new CommonOrderBy();
+            if(StringUtils.isEmpty(currentNodeParentId)){
+                orderBy.add(CommonOrder.asc("e01z101b"));
+                currentNodeName="所有材料";
+                isParent=true;
+            }else {
+                ECatalogTypeInfo eCatalogTypeInfo = new ECatalogTypeInfo();
+                eCatalogTypeInfo=eCatalogTypeService.getByPK(currentNodeId);
+                currentNodeName = eCatalogTypeInfo.getCatalogValue();
+                orderBy.add(CommonOrder.asc("e01Z104"));
+            }
+            List<E01Z1> entities = this.e01Z1Service.list(query, orderBy, pageNum, pageSize);
+            List<E01Z1Vo> vos = new ArrayList<>();
+            E01Z1Vo vo = null;
+            if(entities!=null){
+                for(E01Z1 entity : entities){
+                    vo = new E01Z1Vo();
+                    BeanUtils.copyProperties(vo,entity);
+                    vos.add(vo);
+                }
+            }
+            PagerVo<E01Z1Vo> pager = new PagerVo<E01Z1Vo>(vos, total.intValue(), pageNum, pageSize);
+            map.put("pager", pager);
+            map.put("currentNodeId",currentNodeId);
+            map.put("currentNodeName",currentNodeName);
+            map.put("currentNodeParentId",currentNodeParentId);
+            map.put("a38Id",a38Id);
+            map.put("isParent",isParent);
+            map.put("total",total);
+        }catch(Exception e){
+            logger.error(e);
+            throw new GenericException(e);
+        }
+        
+        return new ModelAndView("saas/zzb/daDemo/manage/mlxxList",map);
+    }
+
+    @RequestMapping(value = "/ajax/mlxxManage")
+    public @ResponseBody ModelAndView mlxxManage(HttpServletRequest request){
+        Map<String, Object> map = Maps.newHashMap();
+        String a38Id = StringUtils.trimNull2Empty(request.getParameter("a38Id"));
+        map.put("a38Id", a38Id);
+        return new ModelAndView("saas/zzb/daDemo/manage/mlxxManage",map);
+    }
+
+    @RequestMapping("/tree")
+    public @ResponseBody
+    Map<String, Object> tree() throws GenericException {
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            CommonConditionQuery query = new CommonConditionQuery();
+            CommonOrderBy orderBy = new CommonOrderBy();
+            orderBy.add(CommonOrder.asc("sort"));
+            List<ECatalogTypeInfo> eCatalogTypeInfos = eCatalogTypeService.list(query, orderBy);
+            List<TreeNode> treeNodes = new ArrayList<TreeNode>();
+            TreeNode treeNode = new TreeNode();
+            treeNode.setId("0");
+            treeNode.setName("目录类型");
+            treeNode.setpId("0");
+            treeNode.setOpen(true);
+            treeNodes.add(treeNode);
+            TreeNode childTreeNode=null;
+            for (ECatalogTypeInfo eCatalogTypeInfo : eCatalogTypeInfos) {
+                childTreeNode = new TreeNode();
+                childTreeNode.setId(eCatalogTypeInfo.getId());
+                childTreeNode.setName(eCatalogTypeInfo.getSort()+"."+eCatalogTypeInfo.getCatalogValue());
+                if(eCatalogTypeInfo.getParent()==null){
+                    childTreeNode.setpId(treeNode.getId());
+                }else{
+                    childTreeNode.setpId(eCatalogTypeInfo.getParent().getId());
+                }
+                treeNodes.add(childTreeNode);
+            }
+            map.put("success", true);
+            map.put("data", treeNodes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e, e);
+            map.put("success", false);
+        }
+        return map;
+    }
+
+}
---
+++ src/main/java/com/hisun/saas/zzb/dzda/mlcl/dao/impl/E01Z1DaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2018. Hunan Hisun Union Information Technology Co, Ltd. All rights reserved.
+ * http://www.hn-hisun.com
+ * 注意:本内容知识产权属于湖南海数互联信息技术有限公司所有,除非取得商业授权,否则不得用于商业目的.
+ */
+
+package com.hisun.saas.zzb.dzda.mlcl.dao.impl;
+
+import com.hisun.base.dao.impl.BaseDaoImpl;
+import com.hisun.saas.sys.tenant.base.dao.imp.TenantBaseDaoImpl;
+import com.hisun.saas.zzb.dzda.mlcl.dao.E01Z1Dao;
+import com.hisun.saas.zzb.dzda.mlcl.entity.E01Z1;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author liuzj {279421824@qq.com}
+ */
+@Repository
+public class E01Z1DaoImpl extends TenantBaseDaoImpl<E01Z1,String>
+       implements E01Z1Dao {
+
+
+}

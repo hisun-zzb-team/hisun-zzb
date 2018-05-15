2face0ba956d82773ab5f6e798611feb92dded96
liuzj1985 <liuzj@30wish.net>
2018-05-15 16:28:27 +0800

    上传图片修改

--- src/main/java/com/hisun/saas/zzb/dzda/mlcl/service/impl/EImagesServiceImpl.java
+++ src/main/java/com/hisun/saas/zzb/dzda/mlcl/service/impl/EImagesServiceImpl.java
@@ -85,7 +85,11 @@ public class EImagesServiceImpl extends BaseServiceImpl<EImages, String>
                     for (E01Z1 e01Z1 : e01z1s) {
                         DecimalFormat decimalFormat = new DecimalFormat("00");
                         String nameCode = decimalFormat.format(e01Z1.getE01Z107());//当前材料对应文件编号
-                        String tpNameCode = tpFile.getName().substring(0,tpFile.getName().lastIndexOf(".")).substring(0, 2);//上传图片文件名编号
+
+                        String tpNameCode = "";
+                        if(tpFile.getName().lastIndexOf(".")!=-1) {
+                            tpNameCode = tpFile.getName().substring(0, tpFile.getName().lastIndexOf(".")).substring(0, 2);//上传图片文件名编号
+                        }
                         if (tpNameCode.equals(nameCode)) {
                             isNeedDelete = false;
                             EImages eImages = new EImages();

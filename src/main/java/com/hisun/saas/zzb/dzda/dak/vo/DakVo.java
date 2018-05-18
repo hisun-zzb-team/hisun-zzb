4ee63ebcf142ebd7e6ace822781928ef59535306
60514 <www19940913>
2018-05-18 11:56:43 +0800

    提交档案库模块代码(高级查询)

---
+++ src/main/java/com/hisun/saas/zzb/dzda/dak/vo/DakVo.java
@@ -0,0 +1,358 @@
+package com.hisun.saas.zzb.dzda.dak.vo;
+
+/**
+ * Created by 60514 on 2018/5/16.
+ */
+public class DakVo {
+    private String a0101;//姓名
+    private String a0157;//单位与职务
+    private String dabh;//档案编号
+    private String smxh;//扫描序号
+    private String gbztCodes;//干部状态字典代码
+    private String gbztContents;//干部状态字典内容
+    private String daztCodes;//档案状态字典代码
+    private String daztContents;//档案状态字典内容
+    private String dutyLevelCode;//职务级别code
+    private String dutyLevelValue;//职务级别
+    private String dutyLevelTimeBase;//职级时间
+    private String dutyLevelTimeBaseStart;//职级时间 开始
+    private String dutyLevelTimeBaseEnd;//职级时间 结束
+    private String a3804B;//档案来处id
+    private String a3804A;//档案来处名称（转入单位名称）
+    private String a3821;//转去单位
+    private String a3801;//档案接收日期
+    private String a3801Start;//档案接收日期 开始
+    private String a3801End;//档案接收日期 结束
+    private String a3817;//档案转出日期
+    private String a3817Start;//档案转出日期 开始
+    private String a3817End;//档案转出日期 结束
+    private String e01Z111;//材料名称
+    private String eCatalogTypeId;//材料类别主键
+    private String e01Z101B;//材料类别字典代码
+    private String e01Z101A;//材料类别字典内容
+    private String e01Z207;//接收人姓名
+    private String e01Z201;//材料接收时间
+    private String e01Z201Start;//材料接收时间 开始
+    private String e01Z201End;//材料接收时间 结束
+    private String e01Z204;//材料来处
+    private String e01z1CreatedTime;//材料创建时间
+    private String e01z1CreatedTimeStart;//材料创建时间 开始
+    private String e01z1CreatedTimeEnd;//材料创建时间 结束
+    private String e01Z117;//材料制成时间
+    private String e01Z117Start;//材料制成时间 开始
+    private String e01Z117End;//材料制成时间 结束
+    private String yjztps;//已加载图片数
+    private String yjztpsName;//已加载图片数
+
+    public String getA0101() {
+        return a0101;
+    }
+
+    public void setA0101(String a0101) {
+        this.a0101 = a0101;
+    }
+
+    public String getA0157() {
+        return a0157;
+    }
+
+    public void setA0157(String a0157) {
+        this.a0157 = a0157;
+    }
+
+    public String getDabh() {
+        return dabh;
+    }
+
+    public void setDabh(String dabh) {
+        this.dabh = dabh;
+    }
+
+    public String getSmxh() {
+        return smxh;
+    }
+
+    public void setSmxh(String smxh) {
+        this.smxh = smxh;
+    }
+
+    public String getGbztCodes() {
+        return gbztCodes;
+    }
+
+    public void setGbztCodes(String gbztCodes) {
+        this.gbztCodes = gbztCodes;
+    }
+
+    public String getGbztContents() {
+        return gbztContents;
+    }
+
+    public void setGbztContents(String gbztContents) {
+        this.gbztContents = gbztContents;
+    }
+
+    public String getDaztCodes() {
+        return daztCodes;
+    }
+
+    public void setDaztCodes(String daztCodes) {
+        this.daztCodes = daztCodes;
+    }
+
+    public String getDaztContents() {
+        return daztContents;
+    }
+
+    public void setDaztContents(String daztContents) {
+        this.daztContents = daztContents;
+    }
+
+    public String getDutyLevelCode() {
+        return dutyLevelCode;
+    }
+
+    public void setDutyLevelCode(String dutyLevelCode) {
+        this.dutyLevelCode = dutyLevelCode;
+    }
+
+    public String getDutyLevelValue() {
+        return dutyLevelValue;
+    }
+
+    public void setDutyLevelValue(String dutyLevelValue) {
+        this.dutyLevelValue = dutyLevelValue;
+    }
+
+    public String getDutyLevelTimeBase() {
+        return dutyLevelTimeBase;
+    }
+
+    public void setDutyLevelTimeBase(String dutyLevelTimeBase) {
+        this.dutyLevelTimeBase = dutyLevelTimeBase;
+    }
+
+    public String getA3804B() {
+        return a3804B;
+    }
+
+    public void setA3804B(String a3804B) {
+        this.a3804B = a3804B;
+    }
+
+    public String getA3804A() {
+        return a3804A;
+    }
+
+    public void setA3804A(String a3804A) {
+        this.a3804A = a3804A;
+    }
+
+    public String getA3821() {
+        return a3821;
+    }
+
+    public void setA3821(String a3821) {
+        this.a3821 = a3821;
+    }
+
+    public String getA3801() {
+        return a3801;
+    }
+
+    public void setA3801(String a3801) {
+        this.a3801 = a3801;
+    }
+
+    public String getA3817() {
+        return a3817;
+    }
+
+    public void setA3817(String a3817) {
+        this.a3817 = a3817;
+    }
+
+    public String getE01Z111() {
+        return e01Z111;
+    }
+
+    public void setE01Z111(String e01Z111) {
+        this.e01Z111 = e01Z111;
+    }
+
+    public String geteCatalogTypeId() {
+        return eCatalogTypeId;
+    }
+
+    public void seteCatalogTypeId(String eCatalogTypeId) {
+        this.eCatalogTypeId = eCatalogTypeId;
+    }
+
+    public String getE01Z101B() {
+        return e01Z101B;
+    }
+
+    public void setE01Z101B(String e01Z101B) {
+        this.e01Z101B = e01Z101B;
+    }
+
+    public String getE01Z101A() {
+        return e01Z101A;
+    }
+
+    public void setE01Z101A(String e01Z101A) {
+        this.e01Z101A = e01Z101A;
+    }
+
+    public String getE01Z207() {
+        return e01Z207;
+    }
+
+    public void setE01Z207(String e01Z207) {
+        this.e01Z207 = e01Z207;
+    }
+
+    public String getE01Z201() {
+        return e01Z201;
+    }
+
+    public void setE01Z201(String e01Z201) {
+        this.e01Z201 = e01Z201;
+    }
+
+    public String getE01Z204() {
+        return e01Z204;
+    }
+
+    public void setE01Z204(String e01Z204) {
+        this.e01Z204 = e01Z204;
+    }
+
+    public String getE01z1CreatedTime() {
+        return e01z1CreatedTime;
+    }
+
+    public void setE01z1CreatedTime(String e01z1CreatedTime) {
+        this.e01z1CreatedTime = e01z1CreatedTime;
+    }
+
+    public String getE01Z117() {
+        return e01Z117;
+    }
+
+    public void setE01Z117(String e01Z117) {
+        this.e01Z117 = e01Z117;
+    }
+
+    public String getYjztps() {
+        return yjztps;
+    }
+
+    public void setYjztps(String yjztps) {
+        this.yjztps = yjztps;
+    }
+
+    public String getDutyLevelTimeBaseStart() {
+        return dutyLevelTimeBaseStart;
+    }
+
+    public void setDutyLevelTimeBaseStart(String dutyLevelTimeBaseStart) {
+        this.dutyLevelTimeBaseStart = dutyLevelTimeBaseStart;
+    }
+
+    public String getDutyLevelTimeBaseEnd() {
+        return dutyLevelTimeBaseEnd;
+    }
+
+    public void setDutyLevelTimeBaseEnd(String dutyLevelTimeBaseEnd) {
+        this.dutyLevelTimeBaseEnd = dutyLevelTimeBaseEnd;
+    }
+
+    public String getA3801Start() {
+        return a3801Start;
+    }
+
+    public void setA3801Start(String a3801Start) {
+        this.a3801Start = a3801Start;
+    }
+
+    public String getA3801End() {
+        return a3801End;
+    }
+
+    public void setA3801End(String a3801End) {
+        this.a3801End = a3801End;
+    }
+
+    public String getA3817Start() {
+        return a3817Start;
+    }
+
+    public void setA3817Start(String a3817Start) {
+        this.a3817Start = a3817Start;
+    }
+
+    public String getA3817End() {
+        return a3817End;
+    }
+
+    public void setA3817End(String a3817End) {
+        this.a3817End = a3817End;
+    }
+
+    public String getE01Z201Start() {
+        return e01Z201Start;
+    }
+
+    public void setE01Z201Start(String e01Z201Start) {
+        this.e01Z201Start = e01Z201Start;
+    }
+
+    public String getE01Z201End() {
+        return e01Z201End;
+    }
+
+    public void setE01Z201End(String e01Z201End) {
+        this.e01Z201End = e01Z201End;
+    }
+
+    public String getE01z1CreatedTimeStart() {
+        return e01z1CreatedTimeStart;
+    }
+
+    public void setE01z1CreatedTimeStart(String e01z1CreatedTimeStart) {
+        this.e01z1CreatedTimeStart = e01z1CreatedTimeStart;
+    }
+
+    public String getE01z1CreatedTimeEnd() {
+        return e01z1CreatedTimeEnd;
+    }
+
+    public void setE01z1CreatedTimeEnd(String e01z1CreatedTimeEnd) {
+        this.e01z1CreatedTimeEnd = e01z1CreatedTimeEnd;
+    }
+
+    public String getE01Z117Start() {
+        return e01Z117Start;
+    }
+
+    public void setE01Z117Start(String e01Z117Start) {
+        this.e01Z117Start = e01Z117Start;
+    }
+
+    public String getE01Z117End() {
+        return e01Z117End;
+    }
+
+    public void setE01Z117End(String e01Z117End) {
+        this.e01Z117End = e01Z117End;
+    }
+
+    public String getYjztpsName() {
+        return yjztpsName;
+    }
+
+    public void setYjztpsName(String yjztpsName) {
+        this.yjztpsName = yjztpsName;
+    }
+}
